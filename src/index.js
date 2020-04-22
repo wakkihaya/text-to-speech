e0e22ff2161cb9e5639839aedc4e3d30055d9720
wakkihaya <wakkihaya@gmail.com>
2020-04-23 00:22:52 +0900

    front design

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ const dotenv = require('dotenv');
 const textToSpeech = require("@google-cloud/text-to-speech");
 const fs = require('fs-extra');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(express.static('./'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 dotenv.config({ path: '../.env'});
 
+app.get('/',(req,res)=>{
+   res.render('index.html')
+});
+
 app.post('/output',(req,res)=>{
-    var input = req["text"];
+    var input = req.body['msg'];
     main(input);
-})
+    res.redirect('/')
+});
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
 
 const main = async function(value) {
     const client = new textToSpeech.TextToSpeechClient();
@@ -25,6 +38,7 @@ const main = async function(value) {
         },
     };
 
+    console.log("get the value: ", value)
     const [response] = await client.synthesizeSpeech(request);
     fs.writeFileSync(`../output/output.mp3`, response.audioContent, `binary`)
 };
